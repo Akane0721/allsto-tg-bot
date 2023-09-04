ffee2767ea52de5d688659897fc6f059cec4977f
Akane0721 <2891889021@qq.com>
2023-09-04 20:58:42 +0800

    swap

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const TelegramBot = require('node-telegram-bot-api')
 const fetch = require('cross-fetch')
 const express = require('express')
 require('dotenv').config({ path: '.env.local' })
-const { mainMenu } = require('./keyboard')
+const { getWallet, getPrivateKey } = require('./Wallet')
+const { mainMenu, from, to_arb, to_bnb, to_eth, confirm } = require('./keyboard')
+const { initiateSwap, getPrice } = require('./mesonAPI')
 
 const {
     BOT_TOKEN,
     PORT = 3000
 } = process.env
+
+let walletAddress
+const selections = {}
 
 const bot = new TelegramBot(BOT_TOKEN, {
     polling: true,
@@ -20,17 +25,25 @@ bot.getMe().then((me) => {
     console.log(`Bot username: ${me.username}`)
 })
 
-const menu_text = `Welcome to the official Alls.to Telegram! If you have any questions or inquiries, please select the appropriate option below.\n\nSimplify Your Stablecoin Life in Web3:  https://alls.to\n
-        \nIf you need further assistance, please send admin a private message: 
-        \n👨‍💼 Moderator: @MrFish\n🤝 Cooperation: @KaceyK0 
-        `
-
 const app = express()
 app.listen(PORT, async () => {
 
-    bot.onText(/\/start/, (msg) => {
+    bot.onText(/\/start/, async (msg) => {
         const chatId = msg.chat.id
+        const userId = msg.from.id
         const userName = msg.from.username
+        walletAddress = await getWallet(userId)
+        if (!selections[userId]) {
+            selections[userId] = []
+        }
+        selections[userId][4] = walletAddress
+        const menu_text = `Welcome to the official Alls.to Telegram! If you have any questions or inquiries, please select the appropriate option below.
+        \nComplete easier cross-chain collection and payment through:  https://alls.to
+        \nFor your convenience, we have automatically generated a wallet address that belongs only to you:\n${walletAddress}\nPlease feel free to use it. Click “Swap” to start a transaction and click "Show Private Key" to display the private key of this address.\n
+        \nIf you need further assistance, please send admin a private message: 
+        \n👨‍💼 Moderator: @MrFish\n🤝 Cooperation: @KaceyK0
+        \nIf you require assistance with your link or transactions regarding Alls.to, please follow these steps for support:\n1. Join the Alls.to Discord server using the following link: https://discord.gg/alls.to\n2. Click the 'Get Help' button below.
+        `
         bot.sendMessage(chatId, menu_text, mainMenu)
     })
 
@@ -40,24 +53,142 @@ app.listen(PORT, async () => {
         const userName = msg.from.username
         const userId = msg.from.id
         console.log(userId)
+        walletAddress = await getWallet(userId)
+        if (!selections[userId]) {
+            selections[userId] = []
+        }
+        selections[userId][4] = walletAddress
+        const menu_text = `Welcome to the official Alls.to Telegram! If you have any questions or inquiries, please select the appropriate option below.
+        \nComplete easier cross-chain collection and payment through:  https://alls.to
+        \nFor your convenience, we have automatically generated a wallet address that belongs only to you:\n${walletAddress}\nPlease feel free to use it. Click “Swap” to start a transaction and click "Show Private Key" to display the private key of this address.\n
+        \nIf you need further assistance, please send admin a private message: 
+        \n👨‍💼 Moderator: @MrFish\n🤝 Cooperation: @KaceyK0
+        \nIf you require assistance with your link or transactions regarding Alls.to, please follow these steps for support:\n1. Join the Alls.to Discord server using the following link: https://discord.gg/alls.to\n2. Click the 'Get Help' button below.
+        `
         bot.sendMessage(chatId, menu_text, mainMenu)
     })
 
     bot.on('callback_query', async (callbackQuery) => {
         const chatId = callbackQuery.message.chat.id
         const userId = callbackQuery.from.id
+        const messageId = callbackQuery.message.message_id
         const data = callbackQuery.data
 
         if (data === 'coop') {
             bot.sendMessage(chatId, 'To implement an enterprise-level fiat and stablecoin payment solution on web3, please contact @KaceyK0 for further collaboration.')
+        }
+        else if (data === 'swap') {
+            bot.sendMessage(chatId, "Please select the origin network and token:", from)
+        }
+        else if (data === 'privateKey') {
+            const privateKey = await getPrivateKey(userId)
+            bot.sendMessage(chatId, `⛔️Warning: Never disclose this key. Anyone with your private keys can steal any assets held in your account.\n\n${privateKey}`)
+        }
+        else if (data.startsWith('from')) {
+            if (!selections[userId]) {
+                selections[userId] = []
+            }
+            selections[userId][0] = data.split('-')[1]
+            selections[userId][1] = data.split('-')[2]
+            let to
+            if (selections[userId][0] === 'eth') to = to_eth
+            else if (selections[userId][0] === 'bnb') to = to_bnb
+            else if (selections[userId][0] === 'arb') to = to_arb
+
+            if (!selections[userId][4]) selections[userId][4] = await getWallet(userId)
+            bot.sendMessage(chatId, `ℹ️The recipient address is ${selections[userId][4]}.\nPlease select the destination network and token:`, to)
+        }
+        else if (data.startsWith('to')) {
+            if (!selections[userId]) {
+                selections[userId] = []
+            }
+            selections[userId][2] = data.split('-')[1]
+            selections[userId][3] = data.split('-')[2]
+            console.log("from: " + selections[userId][2] + "-" + selections[userId][3])
+            bot.sendMessage(chatId, 'Please enter swap amount:', {
+                reply_markup: {
+                    force_reply: true,
+                }
+            })
+        }
+        else if (data === 'continue') {
+            bot.sendMessage(chatId, 'Processing...')
+            try {
+                const privateKey = await getPrivateKey(userId)
+                const swapResp = await initiateSwap(selections[userId], privateKey)
+                console.log(swapResp)
+                bot.sendMessage(chatId, `Swap Successfully. Click the button below to check details.\nSwap ID: ${swapResp.result.swapId}`, {
+                    reply_markup: {
+                        inline_keyboard: [
+                            [{ text: 'Check Status', url: `https://explorer.meson.fi/swap/${swapResp.result.swapId}` }]
+                        ]
+                    }
+                })
+            } catch (e) {
+                bot.sendMessage(chatId, `${e}`)
+                if (e.message === "amount-over-balance") {
+                    bot.sendMessage(chatId, `⚠️ Please make sure that there is enough balance in your wallet address and that it is approved on MesonFi.\n\nFollow these steps to complete the approval operation on MesonFi:\n1. Connect your wallet address to meson.fi.\n2. Enter the corresponding swap amount and click the Swap button.\n3. Click the Approve button on the Approval pop-up window.
+                    `, {
+                        reply_markup: {
+                            inline_keyboard: [
+                                [{ text: 'Approve Now', url: 'https://meson.fi' }]
+                            ]
+                        }
+                    })
+                }
+                else if (e.message === "insufficient-allowance") {
+                    bot.sendMessage(chatId, `⚠️ Please follow these steps to complete the approval operation on MesonFi before swapping:\n1. Connect your wallet address to meson.fi.\n2. Enter the corresponding swap amount and click the Swap button.\n3. Click the Approve button on the Approval pop-up window.
+                    `, {
+                        reply_markup: {
+                            inline_keyboard: [
+                                [{ text: 'Approve Now', url: 'https://meson.fi' }]
+                            ]
+                        }
+                    })
+                }
+            }
+        }
+        else if (data === 'close') {
+            bot.deleteMessage(chatId, messageId)
         }
 
         console.log(`${userId}: ${data}`)
     })
 
     bot.on('message', (msg) => {
-        if (!msg.text || !msg.text.startsWith('/start') && !msg.text.startsWith('/menu')) {
+        if (!msg.text || !msg.text.startsWith('/start') && !msg.text.startsWith('/menu') && !msg.reply_to_message) {
             bot.sendMessage(msg.chat.id, 'Please click a button above to start.')
         }
     })
+
+    bot.on('message', async (msg) => {
+        if (msg.reply_to_message && msg.reply_to_message.text === "Please enter swap amount:") {
+            const chatId = msg.chat.id
+            const userId = msg.from.id
+            if (!msg.text) {
+                bot.sendMessage(chatId, 'Please enter text message')
+                return
+            }
+            const amount = msg.text
+            if (!selections[userId]) {
+                selections[userId] = []
+            }
+            selections[userId][5] = amount
+
+            if (!selections[userId][4]) selections[userId][4] = await getWallet(userId)
+            bot.sendMessage(chatId, `Please confirm the swap details:\nFrom/To: ${selections[userId][0]} (${selections[userId][1]}) → ${selections[userId][2]} (${selections[userId][3]})\nAmount: ${selections[userId][5]} ${selections[userId][1]}\nSender: ${selections[userId][4]}\nRecipient: ${selections[userId][4]}`)
+            const fee = await getPrice(selections[userId])
+            if (!fee.error) {
+                bot.sendMessage(chatId, `Total Fee: ${fee.result.totalFee} ${selections[userId][1]}\nService Fee: ${fee.result.serviceFee} ${selections[userId][1]}\nLP Fee: ${fee.result.lpFee} ${selections[userId][1]}\n\nReceive: ${selections[userId][5] - fee.result.totalFee} ${selections[userId][1]}`, confirm)
+            }
+            else {
+                if (fee.error.message === "invalid-amount") {
+                    bot.sendMessage(chatId, "⚠️Invalid amount. Please enter a number.")
+                    return
+                }
+                bot.sendMessage(chatId, `Error: ${fee.error.message}`)
+            }
+        }
+    })
+
 })
--- keyboard.js
+++ keyboard.js
@@ -3,15 +3,123 @@ const mainMenu = {
         inline_keyboard: [
             [{ text: '👨‍💼 Get Help', url: 'https://discord.com/invite/allsto' }],
             [
+                { text: '🔁Swap', callback_data: 'swap' },
+                { text: '🔑Show Private Key', callback_data: 'privateKey' }
+            ],
+            [
                 { text: '▶️ Create My Payment Link', url: 'https://alls.to' },
                 { text: '📋 Docs', url: 'https://docs.alls.to' }
             ],
-            [{ text: '🤝 Integrate with AllsTo Enterprise', callback_data: 'coop' }],
+            [{ text: '🤝 Integrate with AllsTo Enterprise', callback_data: 'coop' }],           
         ]
     }
 }
 
+const from = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                { text: '✖️ Close', callback_data: 'close' }
+            ],
+            [{text:'---------------- Ethereum ----------------',callback_data:'-'}],
+            [
+                { text: 'USDC', callback_data: 'from-eth-usdc' },
+                { text: 'USDT', callback_data: 'from-eth-usdt' },
+                { text: 'BUSD', callback_data: 'from-eth-busd' }
+            ],
+            [{text:'---------------- BNB Chain ----------------',callback_data:'-'}],
+            [
+                { text: 'USDC', callback_data: 'from-bnb-usdc' },
+                { text: 'USDT', callback_data: 'from-bnb-usdt' },
+                { text: 'BUSD', callback_data: 'from-bnb-busd' }
+            ],
+            [{text:'---------------- Arbitrum ----------------',callback_data:'-'}],
+            [
+                { text: 'USDC', callback_data: 'from-arb-usdc' },
+                { text: 'USDT', callback_data: 'from-arb-usdt' }
+            ]
+        ]
+    }
+}
+
+const to_eth = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                { text: '✖️ Close', callback_data: 'close' }
+            ],
+            [{text:'---------------- BNB Chain ----------------',callback_data:'-'}],
+            [
+                { text: 'USDC', callback_data: 'to-bnb-usdc' },
+                { text: 'USDT', callback_data: 'to-bnb-usdt' },
+                { text: 'BUSD', callback_data: 'to-bnb-busd' }
+            ],
+            [{text:'---------------- Arbitrum ----------------',callback_data:'-'}],
+            [
+                { text: 'USDC', callback_data: 'to-arb-usdc' },
+                { text: 'USDT', callback_data: 'to-arb-usdt' }
+            ]
+        ]
+    }
+}
+
+const to_bnb = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                { text: '✖️ Close', callback_data: 'close' }
+            ],
+            [{text:'---------------- Ethereum ----------------',callback_data:'-'}],
+            [
+                { text: 'USDC', callback_data: 'to-eth-usdc' },
+                { text: 'USDT', callback_data: 'to-eth-usdt' },
+                { text: 'BUSD', callback_data: 'to-eth-busd' }
+            ],
+            [{text:'---------------- Arbitrum ----------------',callback_data:'-'}],
+            [
+                { text: 'USDC', callback_data: 'to-arb-usdc' },
+                { text: 'USDT', callback_data: 'to-arb-usdt' }
+            ]
+        ]
+    }
+}
+
+const to_arb = {
+    reply_markup: {
+        inline_keyboard: [
+            [
+                { text: '✖️ Close', callback_data: 'close' }
+            ],
+            [{text:'---------------- Ethereum ----------------',callback_data:'-'}],
+            [
+                { text: 'USDC', callback_data: 'to-eth-usdc' },
+                { text: 'USDT', callback_data: 'to-eth-usdt' },
+                { text: 'BUSD', callback_data: 'to-eth-busd' }
+            ],
+            [{text:'---------------- BNB Chain ----------------',callback_data:'-'}],
+            [
+                { text: 'USDC', callback_data: 'to-bnb-usdc' },
+                { text: 'USDT', callback_data: 'to-bnb-usdt' },
+                { text: 'BUSD', callback_data: 'to-bnb-busd' }
+            ],
+        ]
+    }
+}
+
+const confirm = {
+    reply_markup: {
+        inline_keyboard: [
+            [{ text: 'Confirm', callback_data: 'continue' }],
+            [{ text: '✖️ Close', callback_data: 'close' }]
+        ]
+    }
+}
 
 module.exports = {
     mainMenu,
+    from,
+    to_arb,
+    to_bnb,
+    to_eth,
+    confirm
 }
